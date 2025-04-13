f66e83e56eb8f0d56870e332d4bad59740c3a6dd
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-04-13 10:46:29 +0700

    Time: 115 ms (78.96%), Space: 18.1 MB (49.15%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -13,6 +13,12 @@ class Solution:
             # *REVIEW THIS LINE*
             maxRepeatLetterCount = max(maxRepeatLetterCount, charFrequency[endChar])
 
+
+            # current window size is from windowStart to windowEnd, overall we have a letter which is
+            # repeating maxRepeatLetterCount times, this means we can have a window which has one letter
+            # repeating maxRepeatLetterCount times and the remaining letters we should replace
+            # if the remaining letters are more than k, it is the time to shrink the window as we
+            # are not allowed to replace more than k letters
             if (windowEnd - windowStart + 1 - maxRepeatLetterCount) > k:
                 startChar = s[windowStart]
                 charFrequency[startChar] -= 1
