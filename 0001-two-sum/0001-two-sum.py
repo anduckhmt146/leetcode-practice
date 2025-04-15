48b775b9075b4999d635fa26ca4b735aebf6485f
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-04-15 09:05:57 +0700

    Time: 0 ms (100%), Space: 18.8 MB (62.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,23 +5,34 @@ class Solution:
         # O(N * logN)
         
         # Pair each number with its index
-        nums_with_indices = list(enumerate(nums))  # [(index, value)]
+        # nums_with_indices = list(enumerate(nums))  # [(index, value)]
 
-        # Sort based on the values
-        nums_with_indices.sort(key=lambda x: x[1])
+        # # Sort based on the values
+        # nums_with_indices.sort(key=lambda x: x[1])
 
-        start, end = 0, len(nums_with_indices) - 1
+        # start, end = 0, len(nums_with_indices) - 1
 
-        while start < end:
-            curr_sum = nums_with_indices[start][1] + nums_with_indices[end][1]
+        # while start < end:
+        #     curr_sum = nums_with_indices[start][1] + nums_with_indices[end][1]
             
-            if curr_sum == target:
-                return [nums_with_indices[start][0], nums_with_indices[end][0]]
-            elif curr_sum < target:
-                start += 1
-            else:
-                end -= 1
+        #     if curr_sum == target:
+        #         return [nums_with_indices[start][0], nums_with_indices[end][0]]
+        #     elif curr_sum < target:
+        #         start += 1
+        #     else:
+        #         end -= 1
 
+        # return [-1, -1]
+
+        # O(N)
+        numToIndex = {}
+        for pE in range(0, len(nums)):
+            currVal = nums[pE]
+
+            if target - currVal in numToIndex:
+                return [pE, numToIndex[target - currVal]]
+            
+            numToIndex[currVal] = pE
         return [-1, -1]
 
 
