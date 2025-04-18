11e35009fc45bd4e9789853fae1f40baf8d92fcc
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-04-18 18:04:27 +0700

    Time: 0 ms (100%), Space: 17.9 MB (35.93%) - LeetHub

---
+++ 0874-backspace-string-compare/0874-backspace-string-compare.py
@@ -0,0 +1,15 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        def build(string):
+            stack = []
+            for char in string:
+                if char == '#':
+                    if stack:
+                        stack.pop()
+                else:
+                    stack.append(char)
+            return stack
+
+        return build(s) == build(t)
+            
+        
