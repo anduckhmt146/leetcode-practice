c70d9107d182d064d06395eae231618e66f9990a
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-04-20 01:30:53 +0700

    Time: 63 ms (20.05%), Space: 18.1 MB (41.15%) - LeetHub

---
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        num1ToBool = {}
+        num2ToBool = {}
+
+        for num in nums1:
+            num1ToBool[num] = num1ToBool.get(num, 0) + 1
+        
+        for num in nums2:
+            num2ToBool[num] = num2ToBool.get(num, 0) + 1
+
+        ans1 = []
+        for num in nums1:
+            if num not in num2ToBool and num not in ans1:
+                ans1.append(num)
+
+        ans2 = []
+        for num in nums2:
+            if num not in num1ToBool and num not in ans2:
+                ans2.append(num)
+
+        return [ans1, ans2]
+        
