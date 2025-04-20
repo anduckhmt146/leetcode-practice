a66289d69d789e20939e402f2a51afd535c438ac
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-04-20 09:43:54 +0700

    Time: 3 ms (64.81%), Space: 17.8 MB (73.78%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,13 +1,26 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         # O(N^2)
-        res = []
-        for i in range(0, len(nums)):
-            count = 0
-            for j in range(0, len(nums)):
-                if j != i and nums[j] < nums[i]:
-                    count += 1
-            res.append(count)
+        # res = []
+        # for i in range(0, len(nums)):
+        #     count = 0
+        #     for j in range(0, len(nums)):
+        #         if j != i and nums[j] < nums[i]:
+        #             count += 1
+        #     res.append(count)
 
-        return res
+        # return res
+
+
+        # O(NlogN)
+        sort_num = sorted(nums)
+        numToIndex = {}
+
+        # [1,2,2,3,8]
+        for i, value in enumerate(sort_num):
+            if value not in numToIndex:
+                numToIndex[value] = i
+
+        return [numToIndex[num] for num in nums]
+
         
