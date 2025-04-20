a3760a71aafd34b7e3b7a9696657deea9f7827e0
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-04-20 15:18:41 +0700

    Time: 1 ms (60.74%), Space: 18.8 MB (75.53%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -2,6 +2,7 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         nextNonDup = 0
         pE = 0
+
         while pE < len(nums):
             if pE == 0 or nums[pE] != nums[nextNonDup - 1]:
                 nums[nextNonDup] = nums[pE]
