c757e7709cc02515797f451b8706092659d0745c
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-04-21 12:39:29 +0700

    Time: 11 ms (44.3%), Space: 18.7 MB (56.13%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        farthest = 0
+        nextStep = 0
+        count = 0
+        # Final step do not need to jump
+        for i in range(0, len(nums) - 1):
+            # Previous step can not read here
+            if (i > farthest): 
+                return 0
+
+            # Farthest jump at step i
+            farthest = max(farthest, i + nums[i]) if i + nums[i] < len(nums) else len(nums) - 1
+
+            if i == nextStep:
+                count += 1
+                nextStep = farthest
+        
+        return count
+        
+        
