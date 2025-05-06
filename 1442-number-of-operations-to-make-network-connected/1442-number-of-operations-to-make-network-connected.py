2e378b37ce9f9475b48461640505de886d2c857a
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-05-06 23:33:33 +0700

    Time: 50 ms (33.28%), Space: 32.3 MB (89.25%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.py
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.py
@@ -1,49 +1,26 @@
 from typing import List
 
-class UnionFind:
-    def __init__(self, n):
-        # Initialize parent to itself, rank to 0
-        self.parent = list(range(n))
-        self.rank = [0] * n
-    
-    def find(self, x):
-        if self.parent[x] != x:
-            # Path compression: flatten the structure for faster future queries
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-    
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        
-        if rootX != rootY:
-            # Union by rank: attach the smaller tree under the larger tree
-            if self.rank[rootX] > self.rank[rootY]:
-                self.parent[rootY] = rootX
-            elif self.rank[rootX] < self.rank[rootY]:
-                self.parent[rootX] = rootY
-            else:
-                self.parent[rootY] = rootX
-                self.rank[rootX] += 1
-
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        # If there are fewer than n-1 connections, we can't connect all nodes
         if len(connections) < n - 1:
-            return -1
+            return -1  # Not enough cables
         
-        # Initialize Union-Find structure
-        uf = UnionFind(n)
+        parent = [i for i in range(n)]
         
-        # Process all the connections and unite the nodes
-        for u, v in connections:
-            uf.union(u, v)
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
         
-        # Count the number of connected components
-        components = 0
-        for i in range(n):
-            if uf.find(i) == i:  # If a node is its own parent, it's a root (a separate component)
-                components += 1
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+            if rootX != rootY:
+                parent[rootX] = rootY
         
-        # The number of operations needed is the number of components minus 1
+        for a, b in connections:
+            union(a, b)
+        
+        # Count number of connected components
+        components = len(set(find(i) for i in range(n)))
         return components - 1
