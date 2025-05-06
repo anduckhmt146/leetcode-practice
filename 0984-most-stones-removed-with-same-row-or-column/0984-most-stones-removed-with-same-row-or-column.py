0f46989338f3bf4934e0331a62e86475b5327276
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-05-06 12:37:07 +0700

    Time: 23 ms (64.09%), Space: 18.7 MB (43.9%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.py
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.py
@@ -19,5 +19,11 @@ class Solution:
             col = f"c{y}"
             union(row, col)
 
+        # {'c0': 'c1', 'r0': 'c0', 'c1': 'c2', 'r1': 'c1', 'c2': 'c2', 'r2': 'c2'}
+        print(parent)
         unique_groups = {find(x) for x in parent}
+
+        # {'c2'}
+        # For every node in parent, we find its ultimate root using find(x. Then collect all unique roots
+        print(unique_groups)
         return len(stones) - len(unique_groups)
