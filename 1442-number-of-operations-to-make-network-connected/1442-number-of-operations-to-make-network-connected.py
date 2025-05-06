254b9d8f076ef6fb076cd932dd9a40ededf55c63
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-05-06 12:49:19 +0700

    Time: 43 ms (57.31%), Space: 32.5 MB (74.7%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.py
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.py
@@ -1,39 +1,49 @@
+from typing import List
+
+class UnionFind:
+    def __init__(self, n):
+        # Initialize parent to itself, rank to 0
+        self.parent = list(range(n))
+        self.rank = [0] * n
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            # Path compression: flatten the structure for faster future queries
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        
+        if rootX != rootY:
+            # Union by rank: attach the smaller tree under the larger tree
+            if self.rank[rootX] > self.rank[rootY]:
+                self.parent[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.parent[rootX] = rootY
+            else:
+                self.parent[rootY] = rootX
+                self.rank[rootX] += 1
+
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        if (len(connections) < n - 1):
+        # If there are fewer than n-1 connections, we can't connect all nodes
+        if len(connections) < n - 1:
             return -1
-
-        # Adjacency matrix
-        # graph = [[0] * n for _ in range(n)]
-        # for u, v in connections:
-        #     graph[u].append(v)
-        #     graph[v].append(u)
         
-        # Optimize memory limit exceed from adjacency matrix to adjacency list
-        graph = [[] for _ in range(n)]
+        # Initialize Union-Find structure
+        uf = UnionFind(n)
+        
+        # Process all the connections and unite the nodes
         for u, v in connections:
-            graph[u].append(v)
-            graph[v].append(u)
-
-        def dfs(node, visited):
-            stack = [node]
-            while stack:
-                current = stack.pop()
-                for neighbor in graph[current]:
-                    if not visited[neighbor]:
-                        visited[neighbor] = True
-                        stack.append(neighbor)
+            uf.union(u, v)
         
-        visited = [False] * n
-        num_components = 0
-
+        # Count the number of connected components
+        components = 0
         for i in range(n):
-            if not visited[i]:
-                visited[i] = True
-                dfs(i, visited)
-                num_components += 1
+            if uf.find(i) == i:  # If a node is its own parent, it's a root (a separate component)
+                components += 1
         
-        return num_components - 1
-        
-
-        
+        # The number of operations needed is the number of components minus 1
+        return components - 1
