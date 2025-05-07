5343e8d1ea23d034e7dc2df36c0b94ef35ef0762
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-05-07 22:58:38 +0700

    Time: 18 ms (76.51%), Space: 18.4 MB (42.5%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,31 +1,31 @@
+from typing import List
+
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        if not grid:
+            return 0
+        
+        rows, cols = len(grid), len(grid[0])
+        maxArea = 0
 
-        def dfs(i, j):
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0:
+        # Flip 1 to 0
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 0:
                 return 0
-            
-            # Mark the visited land
-            grid[i][j] = 0
-            
-            # Start with the current land cell
+            grid[r][c] = 0  # mark as visited
+
+            # When first DFS, count it
             area = 1
+            for dr, dc in [(-1,0), (1,0), (0,-1), (0,1)]:
+                area += dfs(r + dr, c + dc)
             
-            # Accumulate area from all adjacent cells (up, down, left, right)
-            area += dfs(i + 1, j)
-            area += dfs(i - 1, j)
-            area += dfs(i, j + 1)
-            area += dfs(i, j - 1)
-            
+            # Area each time count DFS
             return area
 
-        max_area = 0
-        
-        # Iterate over each cell in the grid
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+
+        for i in range(rows):
+            for j in range(cols):
                 if grid[i][j] == 1:
-                    current_area = dfs(i, j)
-                    max_area = max(current_area, max_area)
-        
-        return max_area
+                    maxArea = max(maxArea, dfs(i, j))
+
+        return maxArea
