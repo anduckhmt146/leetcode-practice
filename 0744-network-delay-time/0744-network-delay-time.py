d4e99920159407f119c0b5bde1ec7203b38b128f
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-05-09 08:05:18 +0700

    Time: 398 ms (28.65%), Space: 19.9 MB (27.34%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -1,33 +1,28 @@
+from typing import List
 import heapq
 from collections import defaultdict
 
 class Solution:
-
-    def networkDelayTime(self, times, n, k):
-        # Build adjacency list
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # Build the adjacency list
         graph = defaultdict(list)
         for u, v, w in times:
             graph[u].append((v, w))
 
-        # Dijkstra Min-heap to track the shortest path: (current_cost, current_node)
-        heap = [(0, k)]
-        visited = {}
+        # Min-heap to get the next closest node
+        min_heap = [(0, k)]  # (time, node)
+        visited = set()
+        time_to_reach = {}
 
-        while heap:
-            time, node = heapq.heappop(heap)
-
-            # Skip if already visited
+        while min_heap:
+            time, node = heapq.heappop(min_heap)
             if node in visited:
                 continue
-
-            visited[node] = time
+            visited.add(node)
+            time_to_reach[node] = time
 
             for neighbor, weight in graph[node]:
                 if neighbor not in visited:
-                    heapq.heappush(heap, (time + weight, neighbor))
+                    heapq.heappush(min_heap, (time + weight, neighbor))
 
-        # k = 2, value: {2: 0, 1: 1, 3: 1, 4: 2}
-        print(visited)
-
-        # If all nodes are visited, return the max time; else, -1
-        return max(visited.values()) if len(visited) == n else -1
+        return max(time_to_reach.values()) if len(visited) == n else -1
