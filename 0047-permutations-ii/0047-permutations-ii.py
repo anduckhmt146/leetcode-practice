d99abfce04e9e2d5a2df1b42ba9ee4f172b5dc65
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-05-09 22:04:50 +0700

    Time: 0 ms (100%), Space: 17.9 MB (77.51%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,32 +1,24 @@
+from typing import List
+
 class Solution:
-    # Think state as abstraction, not the detail flows
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        nums.sort()  # Sort to handle duplicates
-        used = [False] * len(nums)
-        
-        def dfs(start_index, path):
-            if start_index == len(nums):  # is_leaf condition
-                # NOTE: Backtrack when reach the final state
-                ans.append(path[:])  # Add a copy of the path to results
+        result = []
+        nums.sort()  # Sort to group duplicates together
+
+        def backtrack(nums, i):
+            if i == len(nums):
+                result.append(nums)
                 return
-            
-            for i in range(len(nums)):  # get_edges
-                if used[i]:  # prune invalid edges
-                    # NOTE: Backtrack in the middle state
-                    continue
-                # i - 1 is parent, i is current
-                # NOTE: Prunning when duplicate 1 1 1 1, select others 1 is not have any meanings
-                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
-                    continue  # Skip duplicates
-                
-                path.append(nums[i])  # path.add(edge)
-                used[i] = True  # update additional states
-                
-                dfs(start_index + 1, path)  # Recursive call
-                
-                path.pop()  # revert additional states
-                used[i] = False
-        
-        dfs(0, [])
-        return ans
+
+            used = set()
+            for j in range(i, len(nums)):
+                if nums[j] in used:
+                    continue  # Skip duplicates at this level
+
+                used.add(nums[j])
+                new_nums = nums[:]  # make a copy
+                new_nums[i], new_nums[j] = new_nums[j], new_nums[i]
+                backtrack(new_nums, i + 1)
+
+        backtrack(nums, 0)
+        return result
