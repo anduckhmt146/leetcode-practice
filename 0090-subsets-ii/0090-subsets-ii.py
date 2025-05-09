948b302abdc970959a41fca8103c3185bbed40e7
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-05-10 00:36:21 +0700

    Time: 0 ms (100%), Space: 18 MB (36.78%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,30 +1,17 @@
-        #               []
-        #              /  \
-        #           [1]    []
-        #          /  \    /  \
-        #      [1,2]  [1] [2]  []
-        #      /  \   / \  / \  / \
-        # [1,2,2] [1,2] [2,2] [2] [2] []
-
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = set()
+
+        # Giong combination sum nhung khong co diem dung ma lay het
         def backtrack(start, path):
-            # Add the current subset to the result
-            result.append(path[:])
-            
-            # Explore further elements to add to the current subset
+            result.add(tuple(path[:]))  # Add the current subset to the result
             for i in range(start, len(nums)):
-                # Skip duplicates
-                if i > start and nums[i] == nums[i - 1]:
-                    continue
-                # Include nums[i] in the subset
                 path.append(nums[i])
-                # Move on to the next element
+                # Can contain duplicate
                 backtrack(i + 1, path)
-                # Backtrack, remove nums[i] from the current subset
                 path.pop()
 
-        nums.sort()  # Sort the array to handle duplicates
-        result = []
         backtrack(0, [])
-        return result
+        return [list(comb) for comb in result]
+        
