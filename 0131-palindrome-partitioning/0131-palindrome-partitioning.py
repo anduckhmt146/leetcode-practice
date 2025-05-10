8dc7291642940d9d0a5c3633ca0869fa162cc51b
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-05-10 11:19:31 +0700

    Time: 51 ms (54.02%), Space: 34.5 MB (43.92%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,28 +1,24 @@
 from typing import List
 
-# We start at index 0 and try to partition the string at different points.
-# If a substring is a palindrome, we add it to our current partition (path) and recur for the remaining string.
-# Once we reach the end of the string, we store the valid partition.
-# We then backtrack (undo the last addition) to explore other possibilities.
-
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        ans = []
+        result = []
+        current_partition = []
         
         def is_palindrome(sub: str) -> bool:
             return sub == sub[::-1]
-
-        def dfs(start_index: int, path: List[str]):
-            if start_index == len(s):
-                ans.append(path[:])  # Add a copy of the path to the result
+        
+        def backtrack(start: int):
+            if start == len(s):
+                result.append(current_partition[:])
                 return
             
-            for end in range(start_index, len(s)):
-                substring = s[start_index:end + 1]
-                if is_palindrome(substring):  # Prune invalid paths
-                    path.append(substring)
-                    dfs(end + 1, path)
-                    path.pop()  # Backtrack
+            for end in range(start + 1, len(s) + 1):
+                substring = s[start:end]
+                if is_palindrome(substring):
+                    current_partition.append(substring)
+                    backtrack(end)
+                    current_partition.pop()
         
-        dfs(0, [])
-        return ans
+        backtrack(0)
+        return result
