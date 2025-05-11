f1a235d514e20efc4750255f09983b0504550f8a
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-05-11 09:39:58 +0700

    Time: 50 ms (92.88%), Space: 21 MB (81.4%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -5,8 +5,13 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # If both nodes are smaller than the current root, go left.
+        # If both are greater, go right.
+        # Otherwise, you have found the split point, i.e., the Lowest Common Ancestor.
+
         while root:
             if p.val < root.val and q.val < root.val:
                 root = root.left
