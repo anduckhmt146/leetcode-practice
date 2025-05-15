b6ab7b90450e73828d998e8f76172836ff0eef75
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-05-15 21:05:45 +0700

    Time: 3 ms (66.03%), Space: 18 MB (23.91%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        def can_split(max_sum: int) -> bool:
+            count = 1
+            current_sum = 0
+            for num in nums:
+                if current_sum + num > max_sum:
+                    count += 1
+                    current_sum = num
+                else:
+                    current_sum += num
+            return count <= k
+
+        left, right = max(nums), sum(nums)
+        while left < right:
+            mid = (left + right) // 2
+            if can_split(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
