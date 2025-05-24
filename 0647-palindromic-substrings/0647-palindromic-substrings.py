18c61418ed6cbd56800267e4c426289ce43de14f
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-05-24 23:00:17 +0700

    Time: 195 ms (32.76%), Space: 25.6 MB (18.31%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,18 +1,13 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        def expand_around_center(left: int, right: int) -> int:
-            count = 0
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                count += 1
-                left -= 1
-                right += 1
-            return count
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        count = 0
 
-        total = 0
-        for i in range(len(s)):
-            # Odd-length palindromes
-            total += expand_around_center(i, i)
-            # Even-length palindromes
-            total += expand_around_center(i, i + 1)
-
-        return total
+        for end in range(n):
+            for start in range(end + 1):
+                if s[start] == s[end] and (end - start <= 1 or dp[start + 1][end - 1]):
+                    dp[start][end] = True
+                    count += 1
+                    
+        return count
