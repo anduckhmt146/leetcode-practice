95e5f81b4629179b29fe659f6211d3264855b164
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-05-25 09:47:39 +0700

    Time: 770 ms (14.98%), Space: 411.2 MB (7.49%) - LeetHub

---
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.py
@@ -0,0 +1,35 @@
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        m, n = len(str1), len(str2)
+
+        # Step 1: Compute LCS
+        dp = [[""] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(m):
+            for j in range(n):
+                if str1[i] == str2[j]:
+                    dp[i + 1][j + 1] = dp[i][j] + str1[i]
+                else:
+                    dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j], key=len)
+
+        lcs = dp[m][n]
+
+        # Step 2: Merge based on LCS
+        res = []
+        i = j = 0
+        for c in lcs:
+            while i < m and str1[i] != c:
+                res.append(str1[i])
+                i += 1
+            while j < n and str2[j] != c:
+                res.append(str2[j])
+                j += 1
+            res.append(c)
+            i += 1
+            j += 1
+
+        # Append remaining characters
+        res.extend(str1[i:])
+        res.extend(str2[j:])
+        
+        return ''.join(res)
