81847e83e551afafa4b481169fe508f983860a73
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-06-26 17:52:41 +0700

    Time: 38 ms (21.32%), Space: 18.2 MB (82.28%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,21 +1,24 @@
-from typing import List
-
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        def is_valid(board, row, col):
-            # Check column
-            for i in range(row):
-                if board[i][col] == 'Q':
+        board = [['.'] * n for _ in range(n)]
+        res = []
+
+        def isValid(row_index, col_index):
+            # Check col
+            for i in range(n):
+                if board[i][col_index] == 'Q':
                     return False
-            # Check upper-left diagonal
-            i, j = row - 1, col - 1
+            
+            # Check upper-left diagonal (Only check upper row)
+            i, j = row_index - 1, col_index - 1
             while i >= 0 and j >= 0:
                 if board[i][j] == 'Q':
                     return False
                 i -= 1
                 j -= 1
+
             # Check upper-right diagonal
-            i, j = row - 1, col + 1
+            i, j = row_index - 1, col_index + 1
             while i >= 0 and j < n:
                 if board[i][j] == 'Q':
                     return False
@@ -23,17 +26,22 @@ class Solution:
                 j += 1
             return True
 
-        def backtrack(row):
-            if row == n:
-                result.append(["".join(r) for r in board])
+        def backtrack(row_index):
+            if row_index == n:
+                res.append(["".join(r) for r in board])
                 return
-            for col in range(n):
-                if is_valid(board, row, col):
-                    board[row][col] = 'Q'
-                    backtrack(row + 1)
-                    board[row][col] = '.'
 
-        result = []
-        board = [['.'] * n for _ in range(n)]
+            for col_index in range(n):
+                # Step 1: Find '.'
+                # Step 2: Check valid
+                # Step 3: Backtrack
+                if board[row_index][col_index] == '.':
+                    if isValid(row_index, col_index):
+                        board[row_index][col_index] = 'Q'
+                        backtrack(row_index + 1)
+                        board[row_index][col_index] = '.'
+
         backtrack(0)
-        return result
+        return res
+
+
