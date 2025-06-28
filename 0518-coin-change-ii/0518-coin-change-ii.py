e0f35f633eb8718c25b2ed165700eac252622baa
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-06-28 14:55:52 +0700

    Time: 127 ms (74.68%), Space: 18.2 MB (61.86%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,12 +1,14 @@
-from typing import List
-
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+        # Init n of the amount
+        n = len(coins)
+
+        # Count way so init 0
         dp = [0] * (amount + 1)
-        dp[0] = 1  # There's 1 way to make amount 0 (pick nothing)
+        dp[0] = 1 # Do not get any coins
 
         for coin in coins:
-            for i in range(coin, amount + 1):
-                dp[i] += dp[i - coin]
+            for w in range(coin, amount + 1):
+                dp[w] += dp[w - coin]
 
         return dp[amount]
