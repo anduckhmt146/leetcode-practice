44408a1ed7023689dee1e94a4cdaed4d830ba03b
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-06-28 23:08:27 +0700

    Time: 157 ms (36.25%), Space: 78.2 MB (90.26%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,63 @@
+class Node:
+    def __init__(self, key: int, value: int):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}  # key -> Node
+        self.head = None  # LRU
+        self.tail = None  # MRU
+
+    def _remove(self, node: Node):
+        """Remove a node from the linked list."""
+        if node.prev:
+            node.prev.next = node.next
+        else:
+            self.head = node.next  # node was head
+
+        if node.next:
+            node.next.prev = node.prev
+        else:
+            self.tail = node.prev  # node was tail
+
+    def _add_to_tail(self, node: Node):
+        """Add node to the end (MRU)."""
+        node.prev = self.tail
+        node.next = None
+        if self.tail:
+            self.tail.next = node
+        self.tail = node
+
+        if not self.head:
+            self.head = node
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        node = self.cache[key]
+        self._remove(node)
+        self._add_to_tail(node)
+        return node.value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+            self._remove(node)
+            self._add_to_tail(node)
+        else:
+            if len(self.cache) >= self.capacity:
+                # Remove LRU node
+                lru = self.head
+                self._remove(lru)
+                del self.cache[lru.key]
+
+            new_node = Node(key, value)
+            self.cache[key] = new_node
+            self._add_to_tail(new_node)
