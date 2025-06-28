a72e5d440ea42f78688448fd7e1a3ea38a3f0f12
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-06-28 12:12:28 +0700

    Time: 18 ms (96.17%), Space: 17.8 MB (96.22%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -2,18 +2,20 @@ from typing import List
 
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        total_sum = sum(nums)
-        if (total_sum + target) % 2 != 0 or abs(target) > total_sum:
+        if (sum(nums) + target) % 2 != 0 or abs(target) > sum(nums):
             return 0
 
-        subset_sum = (total_sum + target) // 2
+        # Find target_sum is postive, and other is negative 
+        target_sum = (sum(nums) + target) // 2
 
-        # dp[i] = number of ways to reach sum i
-        dp = [0] * (subset_sum + 1)
+        # Trước sau gì cũng duyệt hết nên 1-D được rồi
+        # Count the way to choose num -> get the sum
+        dp = [0] * (target_sum + 1)
         dp[0] = 1  # One way to make sum 0 (pick nothing)
 
         for num in nums:
-            for j in range(subset_sum, num - 1, -1):
-                dp[j] += dp[j - num]
+            for j in range(target_sum, num - 1, -1):
+                dp[j] = dp[j] + dp[j - num]
+            
+        return dp[target_sum]
 
-        return dp[subset_sum]
