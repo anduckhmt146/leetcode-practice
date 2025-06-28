1e86e563b4c41a91a3c191a520884e2c082c8a26
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-06-28 22:51:50 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,24 +1,30 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
-        num = 0
-        sign = '+'  # Initially assume the number is positive
-        s = s.replace(" ", "")  # Remove spaces for easier parsing
+        def helper(chars):
+            num = 0
+            sign = '+'
+            stack = []
+            while chars:
+                ch = chars.pop(0)
 
-        for i, ch in enumerate(s):
-            if ch.isdigit():
-                num = num * 10 + int(ch)
-            if not ch.isdigit() or i == len(s) - 1:
-                if sign == '+':
-                    stack.append(num)
-                elif sign == '-':
-                    stack.append(-num)
-                elif sign == '*':
-                    stack.append(stack.pop() * num)
-                elif sign == '/':
-                    stack.append(int(stack.pop() / num))  # truncate toward zero
-                # ch = '*': previous sign = '+' → push 2 → stack = [3, 2], update sign = '*'
-                sign = ch
-                num = 0
+                if ch.isdigit():
+                    num = num * 10 + int(ch)
 
-        return sum(stack)
+                # not digit -> sign or not chars
+                if ch in '+-*/' or not chars:
+                    if sign == '+':
+                        stack.append(num)
+                    elif sign == '-':
+                        stack.append(-num)
+                    elif sign == '*':
+                        stack.append(stack.pop() * num)
+                    elif sign == '/':
+                        prev = stack.pop()
+                        stack.append(int(prev / num))  # Truncate toward zero
+
+                    sign = ch
+                    num = 0
+
+            return sum(stack)
+
+        return helper(list(s))
