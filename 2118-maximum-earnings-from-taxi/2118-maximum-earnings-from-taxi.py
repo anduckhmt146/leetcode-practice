c4cea22442bac8efeec56dfa2bc7d997f76f3b8b
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-06-28 09:46:05 +0700

    Time: 167 ms (90.63%), Space: 33.4 MB (89.43%) - LeetHub

--- 2118-maximum-earnings-from-taxi/2118-maximum-earnings-from-taxi.py
+++ 2118-maximum-earnings-from-taxi/2118-maximum-earnings-from-taxi.py
@@ -1,22 +1,21 @@
-from bisect import bisect_right
+from bisect import bisect_right 
 
 class Solution:
     def maxTaxiEarnings(self, n: int, rides: List[List[int]]) -> int:
-        # Sort by end_time
-        rides.sort(key=lambda x:x[1]) # Sort by end_time
+        # Sort by end time
+        rides.sort(key=lambda x:x[1])
         m = len(rides)
 
-        # Store the max_value in job i
+        # Create a dp to track maximum until the i
         end_times = [ride[1] for ride in rides]
-        dp = [0] * (m  + 1)
+        dp = [0] * (m + 1)
 
-        # Start from i + 1 -> i
+        # Go backward
         for i in range(1, m + 1):
             start, end, tip = rides[i - 1]
             earning = end - start + tip
-
+            # only find in array
             idx = bisect_right(end_times, start, lo = 0, hi = i - 1)
             dp[i] = max(dp[i - 1], dp[idx] + earning)
-
+        
         return dp[m]
-
