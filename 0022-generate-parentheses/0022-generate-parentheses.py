768bb38754703dc1ce239b953d2b6c0877c797cd
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-07-01 13:14:24 +0700

    Time: 2 ms (34.84%), Space: 17.9 MB (69.47%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,25 +1,17 @@
-from typing import List
-
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        result = []
+        res = []
 
-        def backtrack(current: str, open_count: int, close_count: int):
-            # Base case: if the current string has all pairs
-            if len(current) == 2 * n:
-                result.append(current)
+        def backtrack(curr_str, open_count, close_count):
+            if len(curr_str) == 2 * n:
+                res.append(curr_str)
                 return
-            
-            # Add an opening parenthesis if we still have one
+
             if open_count < n:
-                backtrack(current + "(", open_count + 1, close_count)
-            
-            # Add a closing parenthesis if it won't exceed the number of opens
-            
-            # Why You Don’t See current.pop()
-            # That creates a new string every time (current + "("), so we don’t need to undo        anything — the old current is untouched.
+                backtrack(curr_str + "(", open_count + 1, close_count)
+
             if close_count < open_count:
-                backtrack(current + ")", open_count, close_count + 1)
-        
+                backtrack(curr_str + ")", open_count, close_count + 1)
+
         backtrack("", 0, 0)
-        return result
+        return res
