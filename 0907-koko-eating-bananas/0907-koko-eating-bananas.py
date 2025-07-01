58fa98eb117e486ffa018d72e344d77340e6d840
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-07-01 20:19:37 +0700

    Time: 159 ms (51.83%), Space: 18.8 MB (92.68%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,19 +1,23 @@
-from typing import List
 import math
 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # Define the search space for k
+        # Greedy in range [1, max(piles)]
         left, right = 1, max(piles)
 
-        while left < right:
-            mid = (left + right) // 2
-            # Calculate the total hours it would take at speed mid
-            hours = sum(math.ceil(pile / mid) for pile in piles)
+        def canEat(speed):
+            count = 0
+            for banana in piles:                    
+                count += math.ceil(banana / speed)
 
-            if hours <= h:
-                right = mid  # Try a smaller k
+            return count <= h
+
+        while left <= right:
+            mid = (left + right) // 2
+            if canEat(mid):
+                right = mid - 1
             else:
-                left = mid + 1  # Increase k
+                left = mid + 1
 
         return left
+            
