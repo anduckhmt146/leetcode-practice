654515d142cf1df28a7abb9097e597766f29b2ea
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-07-02 13:13:33 +0700

    Time: 0 ms (100%), Space: 19.1 MB (24.17%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -1,18 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        def dfs(root, total):
-            if root is None:
+        if not root:
+            return False
+
+        def dfs(node, total):
+            if not node:
                 return False
 
-            # Add the current node's value to the total sum
-            total -= root.val
-            
-            # If we are at a leaf node, check if the sum equals targetSum
-            if root.left is None and root.right is None:
-                return total == 0
-            
-            # Otherwise, continue the DFS search for left and right subtrees
-            return dfs(root.left, total) or dfs(root.right, total)
+            # Calculate sum until val
+            total += node.val
 
-        # Miễn chỗ này trả về theo root -> targetSum là được
-        return dfs(root, targetSum)
+            # Only check when find in a leaf node
+            if not node.left and not node.right:
+                return total == targetSum
+
+            left = dfs(node.left, total)
+            right = dfs(node.right, total)
+            return left or right
+
+        return dfs(root, 0)
+            
+
+
+        
