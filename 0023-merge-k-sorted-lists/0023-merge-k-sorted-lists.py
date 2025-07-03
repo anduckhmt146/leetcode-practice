476a4484d326702a854cfc0fdb87b08c27468634
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-07-03 20:49:30 +0700

    Time: 12 ms (48.92%), Space: 20 MB (89.88%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,28 +1,32 @@
-from typing import List, Optional
-
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-        
-    # Optional for printing
-    def __str__(self):
-        return f"{self.val} -> {self.next}"
-
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # Assump we have 50 lists => change it multiple [list1, list2], [list3, list4]
+        # Merge range
+        # Merge 2 lists
         if not lists:
             return None
         return self.merge_range(lists, 0, len(lists) - 1)
+        
 
     def merge_range(self, lists, left, right):
+        # Base case
+        # Mid
+        # Merge 2 list
+
         if left == right:
             return lists[left]
+
         mid = (left + right) // 2
+
         l1 = self.merge_range(lists, left, mid)
         l2 = self.merge_range(lists, mid + 1, right)
         return self.merge_two_lists(l1, l2)
+        
 
     def merge_two_lists(self, l1, l2):
         dummy = ListNode()
@@ -34,6 +38,9 @@ class Solution:
             else:
                 current.next = l2
                 l2 = l2.next
+            
             current = current.next
+
         current.next = l1 if l1 else l2
         return dummy.next
+
