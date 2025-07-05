17d0d4aa3ca3759d49b281c9499ac6034810c1df
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-07-05 12:50:46 +0700

    Time: 18 ms (96%), Space: 17.9 MB (89.62%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -3,18 +3,19 @@ from typing import List
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         total = sum(nums)
-        # Check feasibility
         if (total - target) % 2 != 0 or total < target:
             return 0
         neg = (total - target) // 2
         
-        # Initialize DP array
+        # Init DP
         dp = [0] * (neg + 1)
-        dp[0] = 1  # There's one way to make sum 0: take nothing
-        
+        dp[0] = 1
+
+        # Loop coin
         for num in nums:
-            # Loop backwards to avoid using the same number multiple times
-            for s in range(neg, num - 1, -1):
-                dp[s] += dp[s - num]
-        
+            # Select only 1 => backward
+            for i in range(neg, num - 1, -1):
+                if i >= num:
+                    dp[i] = dp[i] + dp[i - num]
+
         return dp[neg]
