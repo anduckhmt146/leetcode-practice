07be3cdcdca7a6535870ef903fe5cf463fb98ab9
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-07-05 12:11:18 +0700

    Time: 15 ms (99.13%), Space: 18.1 MB (70.61%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -2,20 +2,19 @@ from typing import List
 
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        if (sum(nums) + target) % 2 != 0 or abs(target) > sum(nums):
+        total = sum(nums)
+        # Check feasibility
+        if (total - target) % 2 != 0 or total < target:
             return 0
-
-        # Find target_sum is postive, and other is negative 
-        target_sum = (sum(nums) + target) // 2
-
-        # Trước sau gì cũng duyệt hết nên 1-D được rồi
-        # Count the way to choose num -> get the sum
-        dp = [0] * (target_sum + 1)
-        dp[0] = 1  # One way to make sum 0 (pick nothing)
-
+        neg = (total - target) // 2
+        
+        # Initialize DP array
+        dp = [0] * (neg + 1)
+        dp[0] = 1  # There's one way to make sum 0: take nothing
+        
         for num in nums:
-            for j in range(target_sum, num - 1, -1):
-                dp[j] = dp[j] + dp[j - num]
-            
-        return dp[target_sum]
-
+            # Loop backwards to avoid using the same number multiple times
+            for s in range(neg, num - 1, -1):
+                dp[s] += dp[s - num]
+        
+        return dp[neg]
