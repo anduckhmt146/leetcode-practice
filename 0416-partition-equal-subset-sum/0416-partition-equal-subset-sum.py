85ab15a89edb493ed5bd3c8f05fbfa5bda430943
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-07-05 13:12:22 +0700

    Time: 955 ms (41.99%), Space: 18.4 MB (63.62%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,24 +1,19 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        # Check we can contribute the target // 2 in the current array => If an array can sum to target // 2 in the index i => the rest still target // 2 too
-        if sum(nums) % 2 != 0:
+        # Find dp sum with nums / 2
+        total = sum(nums)
+        if total % 2 != 0:
             return False
 
-        target = sum(nums) // 2
-        
-        # dp[i] means "Can we form a subset of numbers from the list that sums up to exactly i?"
-        dp = [False] * (target + 1)
-        dp[0] = True # No choice subset is 0, we choose []
-        # For example, dp[1] = True if we choose [1]
+        target = total // 2
 
-        # For each num, we try to add it to existing possible sums.
+        dp = [0] * (target + 1)
+        dp[0] = 1
+
         for num in nums:
             for i in range(target, num - 1, -1):
-                dp[i] = dp[i] or dp[i - num]
+                dp[i] += dp[i - num]
 
-        return dp[target]
+        return dp[target] != 0
 
-
-
-
-         
+        
