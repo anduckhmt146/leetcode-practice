e2615af94de220862487eb3218a5297f49e6e88d
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-07-05 09:15:36 +0700

    Time: 0 ms (100%), Space: 17.9 MB (29.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-        
-        # Use two variables to track dp[n-1] and dp[n-2]
-        prev1, prev2 = 2, 1  # prev1 is dp[n-1] and prev2 is dp[n-2]
-        
-        for i in range(3, n + 1):
-            current = prev1 + prev2
-            prev2 = prev1
-            prev1 = current
-        
-        return current
+        # To go to the step i => go 1 step from step i - 1 or go 2 steps from i - 2
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]        
