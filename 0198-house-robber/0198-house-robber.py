ea8b164591b240d99fae8d2a11a4eb8f2e7f1bcc
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-07-06 11:51:37 +0700

    Time: 0 ms (100%), Space: 17.6 MB (82.73%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,21 +1,20 @@
+from typing import List
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        # Find max -> init 0
-        # dp is max value the thief can get until the home i
-        if n == 0:
+        if not nums:
             return 0
-        if n == 1:
+        if len(nums) == 1:
             return nums[0]
 
+        n = len(nums)
         dp = [0] * n
         dp[0] = nums[0]
         dp[1] = max(nums[0], nums[1])
 
         for i in range(2, n):
-            # dp[i - 1] là skip nhà ith
-            # dp[i - 2] + nums[i] là lấy nhà i - 2 và i
+            # Skip the house[i]
+            # Or stole the house[i]
             dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
 
-        return dp[n - 1]
-        
+        return dp[-1]
