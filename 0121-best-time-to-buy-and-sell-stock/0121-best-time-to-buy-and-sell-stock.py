1b554775090f4f6519bf4294aabc09161c632e02
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-07-06 13:52:35 +0700

    Time: 148 ms (7.24%), Space: 27 MB (8.53%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -2,13 +2,15 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = float('inf')  # Initialize to a very high value
-        max_profit = 0
-
-        for price in prices:
-            if price < min_price:
-                min_price = price  # Update minimum price
-            else:
-                max_profit = max(max_profit, price - min_price)  # Potential profit
-
-        return max_profit
+        n = len(prices)
+        if n == 0:
+            return 0
+        
+        dp0 = 0             # Max profit without stock
+        dp1 = -prices[0]    # Max profit with one stock bought
+        
+        for i in range(1, n):
+            dp0 = max(dp0, dp1 + prices[i])  # Sell today or do nothing
+            dp1 = max(dp1, -prices[i])       # Buy today or do nothing
+        
+        return dp0
