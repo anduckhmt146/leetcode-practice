a988e3701ff74c41f5b5aa79e444da4549c53369
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-07-06 10:16:02 +0700

    Time: 754 ms (15.2%), Space: 411.3 MB (5.16%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.py
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.py
@@ -2,7 +2,7 @@ class Solution:
     def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
         m, n = len(str1), len(str2)
 
-        # Step 1: Compute LCS
+        # Step 1: Find the LCS
         dp = [[""] * (n + 1) for _ in range(m + 1)]
 
         for i in range(m):
@@ -10,26 +10,26 @@ class Solution:
                 if str1[i] == str2[j]:
                     dp[i + 1][j + 1] = dp[i][j] + str1[i]
                 else:
-                    dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j], key=len)
+                    dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1], key=len)
 
         lcs = dp[m][n]
 
-        # Step 2: Merge based on LCS
+        # Step 2: Build the SCS using LCS
         res = []
         i = j = 0
         for c in lcs:
-            while i < m and str1[i] != c:
+            while str1[i] != c:
                 res.append(str1[i])
                 i += 1
-            while j < n and str2[j] != c:
+            while str2[j] != c:
                 res.append(str2[j])
                 j += 1
             res.append(c)
             i += 1
             j += 1
 
-        # Append remaining characters
-        res.extend(str1[i:])
-        res.extend(str2[j:])
-        
+        # Add remaining parts
+        res.append(str1[i:])
+        res.append(str2[j:])
+
         return ''.join(res)
