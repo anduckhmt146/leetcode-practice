35678a6610f2cdcf6b8e7f40920e8a6c8794fba7
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-07-07 23:56:29 +0700

    Time: 0 ms (100%), Space: 17.9 MB (41.61%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,6 +1,8 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        digitMapping = {
+        # Subsets = Tree + DFS + Backtrack
+
+        phone = {
             "2": "abc",
             "3": "def",
             "4": "ghi",
@@ -11,20 +13,25 @@ class Solution:
             "9": "wxyz"
         }
 
-        res = []
-
-        # Calculate in the backtrack function
+        result = []
+        
         def backtrack(index, path):
+            # Base case
             if index == len(digits):
-                res.append(path[:])
+                if path:
+                    result.append(''.join(path[:]))
                 return
 
-            keyboardChar = digitMapping[digits[index]]
-            for char in keyboardChar:
-                # Move the logic to backtrack function too
-                backtrack(index + 1, path + char)
+            # Prunning
+            for char in phone[digits[index]]:
+                # Node
+                path.append(char)
 
-        backtrack(0, "")
-        return res if digits != "" else []
+                # Neighbor
+                backtrack(index + 1, path)
+                path.pop()
 
-        
+        backtrack(0, [])
+        return result
+
+            
