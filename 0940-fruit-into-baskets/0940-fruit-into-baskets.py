5b672b84131dbe1ed624cf01ab1b975df762c662
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-07-11 23:43:12 +0700

    Time: 189 ms (43.04%), Space: 23.6 MB (42.64%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.py
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -1,27 +1,26 @@
 class Solution:
-    # it is longest subtring without k character
     def totalFruit(self, fruits: List[int]) -> int:
-        windowStart = 0
-        maxLength = 0
         k = 2
-        fruitFrequency = {}  # store the frequence count of fruit
 
-        # in the following loop we'll try to extend the range [windowStart, windowEnd]
-        for windowEnd in range(0, len(fruits)):
-            typeOfEndFruit = fruits[windowEnd]
-            fruitFrequency[typeOfEndFruit] = fruitFrequency.get(typeOfEndFruit, 0) + 1
-
-            # shrink the window until we are left with k distinct characters 
-            # in the fruitFrequency dictionary
-            while len(fruitFrequency) > k:
-                typeOfStartFruit = fruits[windowStart]
-                fruitFrequency[typeOfStartFruit] -= 1
-            
-                if fruitFrequency[typeOfStartFruit] == 0:
-                    del fruitFrequency[typeOfStartFruit]
-                windowStart += 1
-
-            maxLength = max(maxLength, windowEnd - windowStart + 1)
-
-        return maxLength
+        # Init start
+        start = 0
+        n = len(fruits)
+        state = {}
+        max_len = 0
         
+        # Counting
+        for end in range(n):
+            state[fruits[end]] = state.get(fruits[end], 0) + 1
+
+            # Prunning
+            while len(state) > 2:
+                # Update start
+                state[fruits[start]] -= 1
+                if state[fruits[start]] == 0:
+                    del state[fruits[start]]
+                start += 1
+            
+            max_len = max(max_len, end - start + 1)
+
+        return max_len
+
