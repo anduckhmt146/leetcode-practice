40c67258d26a75290226c75ec67b6f0493322b4f
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-07-15 22:55:45 +0700

    Time: 31 ms (86.83%), Space: 18.2 MB (62.25%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -3,19 +3,23 @@ from typing import List
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         total = sum(nums)
-        if (total - target) % 2 != 0 or total < target:
+        if (total + target) % 2 != 0 or abs(target) > total:
             return 0
-        neg = (total - target) // 2
-        
-        # Init DP
-        dp = [0] * (neg + 1)
-        dp[0] = 1
 
-        # Loop coin
-        for num in nums:
-            # Select only 1 => backward
-            for i in range(neg, num - 1, -1):
-                if i >= num:
-                    dp[i] = dp[i] + dp[i - num]
+        target_sum = (total + target) // 2
+        n = len(nums)
 
-        return dp[neg]
+        # dp[i][j] = number of ways to reach sum j using first i numbers
+        dp = [[0] * (target_sum + 1) for _ in range(n + 1)]
+        dp[0][0] = 1  # One way to reach sum 0 with 0 elements
+
+        for i in range(1, n + 1):
+            num = nums[i - 1]
+            for j in range(target_sum + 1):
+                if j < num:
+                    dp[i][j] = dp[i - 1][j]  # can't pick num
+                else:
+                    # Pick num or skip it
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - num]
+
+        return dp[n][target_sum]
