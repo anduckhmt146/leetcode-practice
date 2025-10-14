00934a2693f18be0010c841371a33ad1b0f43643
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2025-10-14 14:36:41 +0700

    Time: 69 ms (78.32%), Space: 51.3 MB (77.58%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -2,16 +2,15 @@ class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         if not points:
             return 0
-
-        # Sort the balloons based on their ending x-coordinate
+        # sort by interval end
         points.sort(key=lambda x: x[1])
-        
         arrows = 1
-        end = points[0][1]
-
-        for start, curr_end in points[1:]:
-            if start > end:
-                arrows += 1
-                end = curr_end
-
+        arrow_pos = points[0][1]  # shoot at end of first interval
+        for s, e in points[1:]:
+            if s <= arrow_pos <= e:
+                # this interval is already burst by the last arrow
+                continue
+            # need another arrow at this interval's end
+            arrows += 1
+            arrow_pos = e
         return arrows
